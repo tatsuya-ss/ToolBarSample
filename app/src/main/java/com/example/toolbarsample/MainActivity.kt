b2a7ceb0fc4e64e237f6cdfceabaf60521ba7d24
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-30 16:17:11 +0900

    戻るボタンの処理

--- app/src/main/java/com/example/toolbarsample/MainActivity.kt
+++ app/src/main/java/com/example/toolbarsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.toolbarsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.toolbarsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +27,13 @@ class MainActivity : AppCompatActivity() {
         toolBar.background = getDrawable(R.color.purple_200)
         setSupportActionBar(toolBar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            println("戻るボタンが押されました。")
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
